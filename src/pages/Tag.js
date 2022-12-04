cc234e721c546254d4211bdb6ca16100ec09bd9c
Beka Gozalishvili <beqaprogger@gmail.com>
2022-12-04 20:52:46 +0400

    .

--- src/pages/Tag.js
+++ src/pages/Tag.js
@@ -1,12 +1,31 @@
+import React from "react"
 import Blog from "../data";
+import Post from "../components/Post";
+import Posts from "../components/Posts";
 import { useParams } from "react-router-dom";
 
 
 function Tag() {
         const params = useParams();
-        const tagId = params.id;
+        const tagId = parseInt(params.id);
+//const tag = Blog.tags.find(t => t.id == tagId);
+const postsForTag = Blog.posts.filter(post => post.tags.includes(tagId));
+//const postsForTag = Blog.posts.filter(post => post.title != "");
+//console.log(postsForTag);
+  const [post, setPost] = React.useState(postsForTag[0])
+
+  const selectPost = (selected) => {
+    setPost(selected)
+  }
+
+
+
 return (
-<h1>{tagId}</h1>
+<div>
+<h1>{postsForTag.length}</h1>
+      <Posts posts={postsForTag} select={selectPost}/>
+      <Post post={post}/>
+</div>
 )
 }
 
--- src/pages/Tags.js
+++ src/pages/Tags.js
@@ -2,15 +2,27 @@ import Blog from "../data";
 import { Link } from "react-router-dom";
 
 
+Blog.tags.sort((a, b) => {
+a = a.tag.toLowerCase();
+b = b.tag.toLowerCase();
+if (a < b) {
+return -1;
+}
+if (a > b) {
+return 1;
+}
+return 0;
+});
+
 function Tags() {
 return (
 <div>
 <h1>Список тегов</h1>
     <ul>
 {
-  Blog.tags.sort().map((tagName, tagID) => (
-      <li key={tagID}>
-<Link to={`/tags/${tagID}`}>{tagName}</Link>
+  Blog.tags.map((tag) => (
+      <li key={tag.id}>
+<Link to={`/tags/${tag.id}`}>{tag.tag}</Link>
 </li>
   )
 )
