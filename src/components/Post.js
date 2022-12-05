5e6db244c8262dba4ff6d0c1815c207fc3f39280
Beka Gozalishvili <beqaprogger@gmail.com>
2022-12-05 15:09:22 +0400

    .

--- src/components/Post.js
+++ src/components/Post.js
@@ -17,7 +17,7 @@ if (postTitleHeading.current != null)
     <div className="post">
 {
 props.showLink &&
-      <h2><Link to={`/post/${props.post.id}`} state={{post: props.post}}>{props.post.title}</Link></h2> || 
+      <h2><Link to={`/post/${props.post.id}`}>{props.post.title}</Link></h2> || 
       <h2 tabindex="-1" ref={postTitleHeading}>{props.post.title}</h2>
 }
       <p>{new Intl.DateTimeFormat('ru-RU', {dateStyle: 'full', timeStyle: 'short'}).format(props.post.timestamp * 1000)}</p>
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,13 +1,13 @@
-import { useLocation } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import Post from "../components/Post";
 import Posts from "../components/Posts";
 import Blog from "../data.json"
 import React from "react"
 
 function PostPage() {
-  const location = useLocation()
-  const { post } = location.state
-
+  const params = useParams()
+  const postId = parseInt(params.id);
+const post = Blog.posts.filter(p => p.id === postId)[0];
   return (
     <div className="SinglePost">
       <Post post={post} showLink={false}/>
