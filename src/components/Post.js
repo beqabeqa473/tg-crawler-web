e77811e204188fce12ca355d3714d19d883e439b
Beka Gozalishvili <beqaprogger@gmail.com>
2022-12-05 11:57:12 +0400

    focus on heading

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+import { useRef, useEffect } from "react";
 import React from "react";
 import Blog from "../data.json"
 
 const Post = (props) => {
+    const postTitleHeading = useRef(null);
+
+    useEffect(() => {
+if (postTitleHeading.current != null)
+{
+        postTitleHeading.current.focus();
+}
+    }, []);
+
   return (
     <div className="post">
 {
 props.showLink &&
       <h2><Link to={`/post/${props.post.id}`} state={{post: props.post}}>{props.post.title}</Link></h2> || 
-      <h2>{props.post.title}</h2>
+      <h2 tabindex="-1" ref={postTitleHeading}>{props.post.title}</h2>
 }
       <p>{new Intl.DateTimeFormat('ru-RU', {dateStyle: 'full', timeStyle: 'short'}).format(props.post.timestamp * 1000)}</p>
       <p dangerouslySetInnerHTML={{__html: props.post.text}}/>
